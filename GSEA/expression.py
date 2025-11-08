c924ddc53aa65322371618bfaf84b2cd568b50cb
lwl1112 <lwl1112@hotmail.com>
2025-11-08 16:35:18 -0500

    gsea

--- GSEA/expression.py
+++ GSEA/expression.py
@@ -4,12 +4,17 @@
 import numpy as np
 import pandas as pd
 
+
+
 def combinedf(path,names):
-    c1=pd.read_csv(path+names[0]+'.csv')
-    c2=pd.read_csv(path+names[1]+'.csv')
+    c1=pd.read_csv(f'{path}/{names[0]}_correction.csv')[['gene_id','gene_name','correction']]
+    c1.columns=['gene_id','gene_name',names[0]]
+    c2=pd.read_csv(f'{path}/{names[1]}_correction.csv')[['gene_id','gene_name','correction']]
+    c2.columns=['gene_id','gene_name',names[1]]
     cc=pd.merge(c1,c2, on=['gene_id','gene_name']) #
     for i in range(2,len(names)):
-        c=pd.read_csv(path+names[i]+'.csv')
+        c=pd.read_csv(f'{path}/{names[i]}_correction.csv')[['gene_id','gene_name','correction']]
+        c.columns=['gene_id','gene_name',names[i]]
         cc = pd.merge(cc,c, on=['gene_id','gene_name'])
     
     namelist=['gene_id','gene_name']
@@ -19,35 +24,38 @@ def combinedf(path,names):
 
     return cc
 
-pbmctpm=pd.read_csv('../../csv_files/controls34TPM.csv')
+cfoncoXpress_dir='csv_files/prediction'
+herberts=pd.read_csv('csv_files/herberts_meta.txt',sep='\s+',header=None)[1].values
+correction = combinedf(cfoncoXpress_dir, herberts)
+
+
+pbmctpm=pd.read_csv('csv_files/controls34TPM.csv')
 pbmclog2tpm=pbmctpm.drop(columns=['gene_id','gene_name'])
 pbmclog2tpm = np.log2(pbmclog2tpm+1)
 pbmclog2tpm['gene_id']=pbmctpm['gene_id']
 pbmclog2tpm['gene_name']=pbmctpm['gene_name']
 #pbmclog2tpm
 
-#herbertspath = 'scratch/wel4007/cfNDA/herberts/exp/csv_files/prediction/'
-#herbertsnames=pd.read_csv('../../csv_files/herberts_samples.txt',header=None)[0].values
-#herberts_df = combinedf(herbertspath, herbertsnames)
-#herberts_df.to_csv('../../csv_files/herberts63_prediction.csv', index=False)
 
-herberts_df = pd.read_csv('../../csv_files/herberts63_prediction.csv')
+names=pd.read_csv('csv_files/MBCnames',header=None)[0]
+names26 = names[names != 'SRR12587654'].reset_index(drop=True)
+mbc26=combinedf('csv_files/prediction', names26)
 
-log2tpm_df=pd.merge(pbmclog2tpm, herberts_df, on=['gene_id','gene_name'])#mbc_rm1_df, mbc_rm2_df
-log2tpm_df = log2tpm_df .drop('gene_id', axis=1)
-log2tpm_df.index=log2tpm_df['gene_name']
-log2tpm_df.drop('gene_name', axis=1, inplace=True)
 
-df_min_max_scaled = log2tpm_df.copy() 
+##########
+merged_df = pd.merge(pbmclog2tpm, correction, on=['gene_id', 'gene_name'])
+merged_df = pd.merge(merged_df, mbc26, on=['gene_id', 'gene_name'])
+mydf=merged_df.drop(columns=['gene_id','gene_name'])
+mydf.index=merged_df['gene_name']
+
+df_min_max_scaled = mydf.copy() 
   
 # apply normalization techniques 
 for column in df_min_max_scaled.columns: 
     df_min_max_scaled[column] = (df_min_max_scaled[column] - df_min_max_scaled[column].min()) / (df_min_max_scaled[column].max() - df_min_max_scaled[column].min())     
   
-# view normalized data 
-#print(df_min_max_scaled)
-df_min_max_scaled.insert(loc=0, column='DESCRIPTION', value=['NA']*log2tpm_df.shape[0])
+df_min_max_scaled.insert(loc=0, column='DESCRIPTION', value=['NA']*df_min_max_scaled.shape[0])
 df_min_max_scaled.insert(loc=0, column='NAME', value=df_min_max_scaled.index)
-df_min_max_scaled.to_csv('controls34_herberts63.log2TPM.minmax.final.txt',index=False, sep='\t')
+df_min_max_scaled.to_csv('pbmc.prostate.breast26.minmax.txt',index=False, sep='\t')
 
-
+print(df_min_max_scaled.shape)
